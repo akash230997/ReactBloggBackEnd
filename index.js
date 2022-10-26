95a6af35e7d987c71db2de46f36ea1ba0095adf3
AKASH MEENA <akash.meena23@gmail.com>
2022-10-27 01:09:33 +0530

    changes3

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const express = require('express');
 const App = express();
 
 const Router = require('./Router/Data');
+const PORT = process.env.PORT || 9000;
 
 const cors = require('cors');
 
 App.use(cors());
 
-App.use('/apiuser',Router);
+App.use('/user',Router);
 
 
-App.listen(process.env.PORT || 9000, ()=>{
+App.listen(PORT, ()=>{
     console.log('Connected.....');
 });
