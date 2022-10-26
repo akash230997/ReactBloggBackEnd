ee44192e51b0e6d9ba1402dd87ebdb95e056f91c
AKASH MEENA <akash.meena23@gmail.com>
2022-10-27 02:36:14 +0530

    orignal data

--- Router/Data.js
+++ Router/Data.js
@@ -1,24 +1,8 @@
 const express = require('express');
 
-//importing data present in controller
-const storeData = require('../Controller/data');
+const data = require('../Controller/Data');
 
-// connecting all the routes by express.Router();
-const myRouter = express.Router();
+const Router = express.Router();
 
-//creating route and importing data.js and fetching information from controller
-myRouter.route('/fetchData').get(storeData.dataController);
-
-module.exports = myRouter;
-
-
-
-
-// const express = require('express');
-
-// const data = require('../Controller/Data');
-
-// const Router = express.Router();
-
-// Router.route('/api').get(data.control);
-// module.exports = Router;
+Router.route('/api').get(data.control);
+module.exports = Router;
